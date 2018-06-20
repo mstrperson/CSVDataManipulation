347849b184502f84e7db87abb4604f3331330dfb
Jason Cox <coxj@vt.edu>
2018-06-20 10:33:44 -0400

    Initial Commit

---
+++ CSVDataManipulation/ExtendedCSV.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO;
+
+namespace CSVDataManipulation
+{
+    public class ExtendedCSV : CSV
+    {
+        public List<String> UniqueFields
+        {
+            get;
+            set;
+        }
+
+        public ExtendedCSV(Stream inputStream, List<string> uidfields = null) : base(inputStream)
+        {
+            UniqueFields = uidfields == null ? new List<string>() : uidfields;
+
+        }
+    }
+}
