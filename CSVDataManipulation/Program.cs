2a0a53d23f78aacded1378984410910bbcfbb2ce
Jason Cox <coxj@vt.edu>
2018-06-21 08:37:21 -0400

    added configuration file support
    put unique field headers in a .flatten.conf file in the toFlatten directory

--- CSVDataManipulation/Program.cs
+++ CSVDataManipulation/Program.cs
@@ -9,16 +9,46 @@ namespace CSVDataManipulation
     {
         public static void Main(string[] args)
         {
-            List<String> files = Directory.EnumerateFiles(String.Format("/Users/{0}/Documents/toSquash/", Environment.UserName)).ToList();
+            List<String> files = Directory.EnumerateFiles(String.Format("/Users/{0}/Documents/toFlatten/", Environment.UserName)).ToList();
+
             if (!Directory.Exists(String.Format("/Users/{0}/Documents/flattened", Environment.UserName)))
                 Directory.CreateDirectory(String.Format("/Users/{0}/Documents/flattened", Environment.UserName));
+
+            List<String> uniqueFields = new List<string>();
+
+            if(File.Exists(String.Format("/Users/{0}/Documents/toFlatten/.flatten.conf", Environment.UserName)))
+            {
+                StreamReader reader =
+                    new StreamReader(
+                        new FileStream(
+                            String.Format("/Users/{0}/Documents/toFlatten/.flatten.conf", Environment.UserName),
+                            FileMode.Open));
+
+                String fields = reader.ReadLine();
+                uniqueFields.AddRange(fields.Split('\t'));
+            }
+            else
+            {
+                // ask the user.
+                Console.WriteLine("What are the unique fields?  [Enter one at a time.  '*' to End.");
+                String field = "";
+                while(true)
+                {
+                    Console.Write(">> ");
+                    field = Console.ReadLine();
+                    if (field.Equals("*")) 
+                        break;
+                    else
+                        uniqueFields.Add(field);
+                }
+            }
 
             foreach (string fileName in files)
             {
                 if (!fileName.EndsWith(".csv", StringComparison.CurrentCultureIgnoreCase)) continue;
                 Console.WriteLine(fileName);
                 FileStream fileStream = new FileStream(fileName, FileMode.Open);
-                ExtendedCSV extendedCSV = new ExtendedCSV(fileStream, new List<string>() { "id" });
+                ExtendedCSV extendedCSV = new ExtendedCSV(fileStream, uniqueFields);
                 CSV output = extendedCSV.SquashRows();
                 string outfilename = fileName.Replace("toSquash", "flattened");
                 output.Save(outfilename);
