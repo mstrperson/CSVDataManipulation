d9a91a4328cd657874af120cc9db60ccbdcf8f75
Jason Cox <coxj@vt.edu>
2018-07-03 09:53:43 -0400

    Added Mac Address normalization

--- CSVDataManipulation/NormalizationRule.cs
+++ CSVDataManipulation/NormalizationRule.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace CSVDataManipulation
 {
@@ -27,6 +28,76 @@ namespace CSVDataManipulation
                 {
                     row[column] = Capitalize ? row[column].ToUpperInvariant() : row[column].ToLowerInvariant();
                 }
+            }
+        }
+    }
+
+    public class MACAddressNormalizationRule : INormalizationRule
+    {
+        public bool Capitalize { get; set; }
+        public enum MacSeparator
+        {
+            None = 0,
+            Colon = 1,
+            Dash = 2
+        }
+
+        private String[] separators = { "", ":", "-" };
+
+        public MacSeparator Separator { get; set; }
+
+        public void Normalize(ref List<Dictionary<string, string>> data, List<string> columns)
+        {
+            Regex macaddress = new Regex(MatchPattern);
+            foreach(Dictionary<String, String> row in data)
+            {
+                foreach(String column in columns)
+                {
+                    if (macaddress.IsMatch(row[column]))
+                        continue;
+
+                    String stripped = "";
+                    Regex digit = new Regex("[0-9a-fA-F]");
+                    foreach(char ch in row[column])
+                    {
+                        if(digit.IsMatch("" + ch))
+                        {
+                            stripped += ch;
+                        }
+                    }
+
+                    stripped = Capitalize ? stripped.ToUpperInvariant() : stripped.ToLowerInvariant();
+
+                    if (stripped.Length != 12)
+                        throw new FormatException("Invalid MAC Address");
+
+
+                    String formatted = "";
+                    for (int i = 0; i < 12; i+=2)
+                    {
+                        formatted += stripped[i];
+                        formatted += stripped[i + 1];
+                        if (i < 9) formatted += separators[(int)Separator];
+                    }
+
+                    row[column] = formatted;
+                }
+            }
+        }
+
+        protected string MatchPattern
+        {
+            get
+            {
+                string letter = Capitalize ? "A-F" : "a-F";
+                if(Separator == MacSeparator.None)
+                {
+                    return "^[" + letter + "0-9]{12}$";
+                }
+
+                string separator = Separator == MacSeparator.Colon ? ":" : "-";
+
+                return "^([" + letter + "0-9]{2}" + separator + "){5}[" + letter + "0-9]{2}$";
             }
         }
     }
