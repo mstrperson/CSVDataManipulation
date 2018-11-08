940c952248181c6a65debc6c5764ad40b39a6959
Jason Cox <coxj@vt.edu>
2018-11-07 19:51:28 -0500

    ...

--- CSVDataManipulation/ExtendedCSV.cs
+++ CSVDataManipulation/ExtendedCSV.cs
@@ -147,7 +147,7 @@ namespace CSVDataManipulation
                             else if(!rowA[column].Equals(rowB[column]))
                             {
                                 // check for conflicting data.
-                                rowA[column] = conflictRule.Resolve(rowA, rowB, column);
+                                rowA[column] = ConflictRule.Resolve(rowA, rowB, column);
                             }
                         }
                     }
